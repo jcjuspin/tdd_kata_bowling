509b2e125b882ae3cef81b113c66ed4fd41d22dc
Jean-Christophe97 <juspinjean@gmail.com>
2020-10-19 19:46:00 +0200

    refactor: improve game design

--- src/bowling.test.ts
+++ src/bowling.test.ts
@@ -8,12 +8,20 @@ describe('BowlingGame Test', () => {
 
   test('should score 0 for gutter game', () => {
     rollMany(20,0)
-    expect(game.score).toEqual(0)
+    expect(game.score()).toEqual(0)
   })
 
   test('should score 20 for all ones game', () => {
     rollMany(20,1)
-    expect(game.score).toEqual(20)
+    expect(game.score()).toEqual(20)
+  })
+
+  test('should score 16 with a spare followed by a 3 ball', () => {
+    game.roll(5)
+    game.roll(5)
+    game.roll(3)
+    rollMany(17,0)
+    expect(game.score()).toEqual(16)
   })
 
   function rollMany (n: number, pins: number): void {
