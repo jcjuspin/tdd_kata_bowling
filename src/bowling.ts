d16dfaecfef079cac3f823aacc8ec4abaabfdb4a
Jean-Christophe97 <juspinjean@gmail.com>
2020-10-19 19:58:04 +0200

    refactor: extract method to verify a spare

--- src/bowling.ts
+++ src/bowling.ts
@@ -6,12 +6,12 @@ export class Game {
     let score = 0
     let frameIndex = 0
     for (let frame = 0; frame < 10; frame++) {
-      if (this.rolls[frameIndex] + this.rolls[frameIndex + 1] == 10) {
+      if (this.isSpare(frameIndex)) {
         score += 10 + this.rolls[frameIndex + 2]
         frameIndex += 2
       } else {
-      score += this.rolls[frameIndex] + this.rolls[frameIndex + 1]
-      frameIndex += 2;
+        score += this.rolls[frameIndex] + this.rolls[frameIndex + 1]
+        frameIndex += 2;
       }
     }
     return score
@@ -19,5 +19,9 @@ export class Game {
 
   roll (pins: number): void {
     this.rolls[this.currentRoll++] = pins
+  }
+
+  isSpare(frameIndex: number): boolean {
+    return this.rolls[frameIndex] + this.rolls[frameIndex + 1] == 10
   }
 }
