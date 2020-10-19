37247c35399eaa634138a49889e0348e3fd6f7f0
Jean-Christophe97 <juspinjean@gmail.com>
2020-10-19 19:24:21 +0200

    test: all ones game

--- src/bowling.test.ts
+++ src/bowling.test.ts
@@ -7,4 +7,12 @@ describe('BowlingGame Test', () => {
     }
     expect(game.score()).toEqual(0)
   })
+
+  test('should score 20 for all ones game', () => {
+    const game: Game = new Game()
+    for (let i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score()).toEqual(20)
+  })
 })
