3d5f2a0d15dcf3c87774fc5ebefd3d556b9daa9c
Jean-Christophe97 <juspinjean@gmail.com>
2020-10-19 19:51:46 +0200

    feat: spare score

--- src/bowling.ts
+++ src/bowling.ts
@@ -6,8 +6,13 @@ export class Game {
     let score = 0
     let frameIndex = 0
     for (let frame = 0; frame < 10; frame++) {
+      if (this.rolls[frameIndex] + this.rolls[frameIndex + 1] == 10) {
+        score += 10 + this.rolls[frameIndex + 2]
+        frameIndex += 2
+      } else {
       score += this.rolls[frameIndex] + this.rolls[frameIndex + 1]
       frameIndex += 2;
+      }
     }
     return score
   }
